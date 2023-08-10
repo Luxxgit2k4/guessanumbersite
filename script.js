ba9d15ceb25880b0385e07465f3c86eba500f007
Lakshmanan P <122290398+Luxxgit2k4@users.noreply.github.com>
2023-08-10 21:50:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let secretNumber = Math.trunc(Math.random() * 30) + 1;
+let score = 30;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.start').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('⛔️ No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 30;
+  secretNumber = Math.trunc(Math.random() * 30) + 1;
+
+  displayMessage('Start Guessing......');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = 'black';
+  document.querySelector('.number').style.width = '15rem';
+});
